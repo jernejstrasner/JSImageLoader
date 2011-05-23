17f14560ec591179c2286f557e8dc65cb08c6272
Jernej Strasner <jernej.strasner@gmail.com>
2011-05-23 11:08:29 +0200

    Initial Commit

---
+++ JSImageCache/JSImageCacheAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  JSImageCacheAppDelegate.h
+//  JSImageCache
+//
+//  Created by Jernej Strasner on 5/23/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JSImageCacheAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
---
+++ JSImageCache/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  JSImageCache
+//
+//  Created by Jernej Strasner on 5/23/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController {
+
+}
+
+
+@end
