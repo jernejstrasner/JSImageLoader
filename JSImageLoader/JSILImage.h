10e06dec6d158d7b917345bd94a480b07425d51d
Jernej Strasner <jernej.strasner@gmail.com>
2014-01-28 16:23:28 -0800

    Added new object to hold images

---
+++ JSImageLoader/JSILImage.h
@@ -0,0 +1,16 @@
+//
+//  JSILImage.h
+//  JSImageLoader
+//
+//  Created by Jernej Strasner on 1/28/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JSILImage : NSObject
+
+@property (nonatomic, strong) UIImage *image;
+@property (nonatomic, strong) NSURL *url;
+
+@end
---
+++ JSImageLoader/JSILImage.m
@@ -0,0 +1,13 @@
+//
+//  JSILImage.m
+//  JSImageLoader
+//
+//  Created by Jernej Strasner on 1/28/14.
+//
+//
+
+#import "JSILImage.h"
+
+@implementation JSILImage
+
+@end
