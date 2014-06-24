8c75b5697aa1db10d1bc124ce3a0b382ea4f13f9
Jernej Strasner <jernej.strasner@gmail.com>
2014-06-24 11:29:09 +0200

    Rewrote the app delegate into swift

---
+++ JSImageLoader/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  JSImageLoader
+//
+//  Created by Jernej Strasner on 6/24/14.
+//
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+	var window: UIWindow!
+	
+	func application(application: UIApplication, didFinishLaunchingWithOptions launcOptions: NSDictionary?) -> Bool {
+		return true
+	}
+}
