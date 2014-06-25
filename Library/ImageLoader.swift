217122b3d1d2cd35dfaa333e93fc150099b4acde
Jernej Strasner <jernej.strasner@gmail.com>
2014-06-25 20:01:30 +0200

    Implemented image loading

--- Library/ImageLoader.swift
+++ Library/ImageLoader.swift
@@ -66,7 +66,45 @@ class ImageLoader {
 		downloadQueue.cancelAllOperations()
 	}
 	
-	func getImageAtURL(url: NSURL, completion: (error: NSError, image: UIImage, url: NSURL, cached: Bool)) {
-		
+	func getImageAtURL(url: NSURL, completion: (error: NSError!, image: UIImage!, url: NSURL, cached: Bool) -> Void) {
+		ImageLoaderCache.sharedCache.fetchImage(url) { image in
+			
+			if image {
+				completion(error: nil, image: image, url: url, cached: true)
+			}
+			else {
+				self.downloadQueue.addOperationWithBlock() {
+					let request = NSURLRequest(URL: url)
+					var response: NSURLResponse?
+					var netError: NSError? = nil
+					
+					var counter = NumberOfRetries
+					while counter-- > 0 {
+						let imageData = NSURLConnection.sendSynchronousRequest(request,
+							returningResponse: &response,
+							error: &netError)
+						
+						if netError {
+							switch netError!.code {
+							case NSURLErrorUnsupportedURL, NSURLErrorBadURL, NSURLErrorBadServerResponse, NSURLErrorRedirectToNonExistentLocation, NSURLErrorFileDoesNotExist, NSURLErrorFileIsDirectory:
+								completion(error: netError, image: nil, url: url, cached: false)
+								return
+							default:
+								if counter < 1 {
+									completion(error: netError, image: nil, url: url, cached: false)
+									return
+								}
+							}
+						}
+						else {
+							let loadedImage = UIImage(data: imageData)
+							ImageLoaderCache.sharedCache.cacheImage(loadedImage, url: url)
+							completion(error: nil, image: loadedImage, url: url, cached: false)
+							return
+						}
+					}
+				}
+			}
+		}
 	}
 }
