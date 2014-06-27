3221b597d5c3e556bd9ab2061e7f69dacc0ab18b
Jernej Strasner <jernej.strasner@gmail.com>
2014-06-27 12:11:04 +0200

    Added more tests

--- JSImageLoaderTests/StringDigestTests.swift
+++ JSImageLoaderTests/StringDigestTests.swift
@@ -8,13 +8,19 @@
 
 import XCTest
 
-class StringDigestTests: XCTestCase {
+class ExtensionTests: XCTestCase {
     
 	func testMD5() {
 		let string = "Testiram neki"
 		let hash = "d4f117b9a61ce2541e179113c50fcb1a"
 		
 		XCTAssertEqual(hash, string.md5)
+	}
+	
+	func testNSDateEquatable() {
+		XCTAssertTrue(NSDate() < NSDate(timeIntervalSinceNow: 1))
+		XCTAssertTrue(NSDate() > NSDate(timeIntervalSinceNow: -1))
+		XCTAssertTrue(NSDate() == NSDate(timeIntervalSinceNow: 0))
 	}
 	
 }
